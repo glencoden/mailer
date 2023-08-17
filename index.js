860ce3d645a4b7934c03b2f404cb4054eb827467
glen coden <info@simonmeyer.de>
2023-08-17 22:30:31 +0200

    add cors

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+const cors = require('cors')
+app.use(cors())
+
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
